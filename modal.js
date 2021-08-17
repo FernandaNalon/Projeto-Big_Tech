9e7f359bf6e4babeda7b69d5243ded0e25a2d2c4
Fernanda <78928436+FernandaNalon@users.noreply.github.com>
2021-08-17 13:09:26 -0300

    Delete modal.js

--- modal.js
+++
@@ -1,9 +0,0 @@
-function abrir_modal() {
-    document.querySelector(".caixa").style.display = "block";
-    document.querySelector(".tela_preta").style.display = "block";
-}
-
-function sair() {
-    document.querySelector(".caixa").style.display = "none";
-    document.querySelector(".tela_preta").style.display = "none";
-}
