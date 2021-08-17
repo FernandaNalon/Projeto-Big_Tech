b0100f37b6d81677be38c6c4faba74a42a47f1b1
Fernanda <78928436+FernandaNalon@users.noreply.github.com>
2021-08-17 12:47:07 -0300

    Add files via upload

---
+++ modal.js
@@ -0,0 +1,9 @@
+function abrir_modal() {
+    document.querySelector(".caixa").style.display = "block";
+    document.querySelector(".tela_preta").style.display = "block";
+}
+
+function sair() {
+    document.querySelector(".caixa").style.display = "none";
+    document.querySelector(".tela_preta").style.display = "none";
+}
